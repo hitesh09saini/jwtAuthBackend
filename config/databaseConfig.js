fcb1e9907031a7fcca27412fcd5a53636110fdc2
Hitesh Saini <hitesh9694saini@gmail.com>
2023-12-06 15:47:19 +0530

    push

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -1,6 +1,6 @@
 const mongoose =  require('mongoose')
 
-const path = process.env.DataBase_URl || 'mongodb://127.0.0.1:27017/mydatabase';
+const path = process.env.MONGO_URI|| 'mongodb://127.0.0.1:27017/mydatabase';
 
 const databaseconnect = ()=>{
     mongoose
